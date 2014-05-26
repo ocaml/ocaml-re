066357b2eeedd49bc179bdcdc220e18c5ff29223
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-05-26 01:49:12 -0400

    remove signature from Re.mli also expose Caml_char
    it now resides in Re_intf

--- lib/re_intf.mli
+++ lib/re_intf.mli
@@ -19,13 +19,20 @@ module type String = sig
 end
 
 module type S = sig
-  type t              (* Regular expression *)
   type string
   type char
-  type re             (* Compiled regular expression *)
-  type substrings     (* Match informations *)
 
-  (* Compilation and execution of a regular expression *)
+  type t
+  (** Regular expression *)
+
+  type re
+  (** Compiled regular expression *)
+
+  type substrings
+  (** Match informations *)
+
+  (** {2 Compilation and execution of a regular expression} *)
+
   val compile : t -> re
   val exec :
     ?pos:int ->    (* Default: 0 *)
@@ -40,72 +47,148 @@ module type S = sig
     ?len:int ->    (* Default: -1 (until end of string) *)
     re -> string -> [ `Full | `Partial | `Mismatch ]
 
-  (* Substring extraction *)
-  val get : substrings -> int -> string
-  (* Raise [Not_found] if the group did not match *)
-  val get_ofs : substrings -> int -> int * int
-  (* Raise [Not_found] if the group did not match *)
-  val get_all : substrings -> string array
-  (* Return the empty string for each group which did not match *)
-  val get_all_ofs : substrings -> (int * int) array
-  (* Return [(-1,-1)] for each group which did not match *)
-  val test : substrings -> int -> bool
-  (* Test whether a group matched *)
+  (** {2 Substring extraction} *)
 
-  (* String expressions (literal match) *)
+  val get : substrings -> int -> string
+  (** Raise [Not_found] if the group did not match *)
+
+  val get_ofs : substrings -> int -> int * int
+  (** Raise [Not_found] if the group did not match *)
+
+  val get_all : substrings -> string array
+  (** Return the empty string for each group which did not match *)
+
+  val get_all_ofs : substrings -> (int * int) array
+  (** Return [(-1,-1)] for each group which did not match *)
+
+  val test : substrings -> int -> bool
+  (** Test whether a group matched *)
+
+  (** {2 String expressions (literal match)} *)
+
   val str : string -> t
   val char : char -> t
 
-  (* Basic operations on regular expressions *)
-  val alt : t list -> t                  (* Alternative *)
-  val seq : t list -> t                  (* Sequence *)
-  val empty : t                          (* Match nothing *)
-  val epsilon : t                        (* Empty word *)
-  val rep : t -> t                       (* 0 or more matches *)
-  val rep1 : t -> t                      (* 1 or more matches *)
-  val repn : t -> int -> int option -> t (* Repeated matches *)
-  val opt : t -> t                       (* 0 or 1 matches *)
+  (** {2 Basic operations on regular expressions} *)
 
-  (* String, line, word *)
-  val bol : t                            (* Beginning of line *)
-  val eol : t                            (* End of line *)
-  val bow : t                            (* Beginning of word *)
-  val eow : t                            (* End of word *)
-  val bos : t                            (* Beginning of string *)
-  val eos : t                            (* End of string *)
-  val leol : t                           (* Last end of line or end of string *)
-  val start : t                          (* Initial position *)
-  val stop : t                           (* Final position *)
-  val word : t -> t                      (* Word *)
-  val not_boundary : t                   (* Not at a word boundary *)
-  val whole_string : t -> t              (* Only matches the whole string *)
+  val alt : t list -> t
+  (** Alternative *)
 
-  (* Match semantics *)
-  val longest : t -> t                   (* Longest match *)
-  val shortest : t -> t                  (* Shortest match *)
-  val first : t -> t                     (* First match *)
+  val seq : t list -> t
+  (** Sequence *)
 
-  (* Repeated match modifiers *)
-  val greedy : t -> t                    (* Greedy *)
-  val non_greedy : t -> t                (* Non-greedy *)
+  val empty : t
+  (** Match nothing *)
 
-  (* Groups (or submatches) *)
-  val group : t -> t                     (* Delimit a group *)
-  val no_group : t -> t                  (* Remove all groups *)
+  val epsilon : t
+  (** Empty word *)
+
+  val rep : t -> t
+  (** 0 or more matches *)
+
+  val rep1 : t -> t
+  (** 1 or more matches *)
+
+  val repn : t -> int -> int option -> t
+  (** Repeated matches *)
+
+  val opt : t -> t
+  (** 0 or 1 matches *)
+
+  (** {2 String, line, word} *)
+
+  val bol : t
+  (** Beginning of line *)
+
+  val eol : t
+  (** End of line *)
+
+  val bow : t
+  (** Beginning of word *)
+
+  val eow : t
+  (** End of word *)
+
+  val bos : t
+  (** Beginning of string *)
+
+  val eos : t
+  (** End of string *)
+
+  val leol : t
+  (** Last end of line or end of string *)
+
+  val start : t
+  (** Initial position *)
+
+  val stop : t
+  (** Final position *)
+
+  val word : t -> t
+  (** Word *)
+
+  val not_boundary : t
+  (** Not at a word boundary *)
+
+  val whole_string : t -> t
+  (** Only matches the whole string *)
+
+  (** {2 Match semantics} *)
+
+  val longest : t -> t
+  (** Longest match *)
+
+  val shortest : t -> t
+  (** Shortest match *)
+
+  val first : t -> t
+  (** First match *)
+
+  (** {2 Repeated match modifiers} *)
+
+  val greedy : t -> t
+  (** Greedy *)
+
+  val non_greedy : t -> t
+  (** Non-greedy *)
+
+  (** {2 Groups (or submatches)} *)
+
+  val group : t -> t
+  (** Delimit a group *)
+
+  val no_group : t -> t
+  (** Remove all groups *)
+
   val nest : t -> t
-  (* when matching against [nest e], only the group matching in the
-     last match of e will be considered as matching *)
+  (** when matching against [nest e], only the group matching in the
+      last match of e will be considered as matching *)
 
-  (* Character sets *)
-  val set : string -> t                  (* Any character of the string *)
-  val rg : char -> char -> t             (* Character ranges *)
-  val inter : t list -> t                (* Intersection of character sets *)
-  val diff : t -> t -> t                 (* Difference of character sets *)
-  val compl : t list -> t                (* Complement of union *)
+  (** {2 Character sets} *)
 
-  (* Predefined character sets *)
-  val any : t                            (* Any character *)
-  val notnl : t                          (* Any character but a newline *)
+  val set : string -> t
+  (** Any character of the string *)
+
+  val rg : char -> char -> t
+  (** Character ranges *)
+
+  val inter : t list -> t
+  (** Intersection of character sets *)
+
+  val diff : t -> t -> t
+  (** Difference of character sets *)
+
+  val compl : t list -> t
+  (** Complement of union *)
+
+  (** {2 Predefined character sets} *)
+
+  val any : t
+  (** Any character *)
+
+  val notnl : t
+  (** Any character but a newline *)
+
   val alnum : t
   val alpha : t
   val ascii : t
@@ -120,12 +203,17 @@ module type S = sig
   val upper : t
   val xdigit : t
 
-  (* Case modifiers *)
-  val case : t -> t                      (* Case sensitive matching *)
-  val no_case : t -> t                   (* Case insensitive matching *)
+  (** {2 Case modifiers} *)
+
+  val case : t -> t
+  (** Case sensitive matching *)
+
+  val no_case : t -> t
+  (** Case insensitive matching *)
 
   (****)
 
-  (* Internal debugging *)
+  (** {2 Internal debugging}  *)
+
   val print_re : Format.formatter -> re -> unit
 end
