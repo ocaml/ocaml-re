9cc1f4aa34bfebd673d59a3385726ed5606d1359
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-11-28 01:55:22 -0500

    add ounit based test for pcre posix classes

---
+++ lib_test/test_pcre.ml
@@ -0,0 +1,20 @@
+open OUnit
+
+let test_blank_class _ =
+  let re = Re_perl.compile_pat {|\d[[:blank:]]\d[[:blank:]]+[a-z]|} in
+  let successes = ["1 2  a"; "2\t3 z"; "9\t0 \t a"] in
+  let failures = [""; "123"; "  "; "1 3z"] in
+  successes |> List.iter (fun s ->
+    assert_bool ("String " ^ s ^ " should match") (Re.execp re s)
+  );
+  failures |> List.iter (fun s ->
+    assert_bool ("String " ^ s ^ " should not match") (not @@ Re.execp re s)
+  )
+
+
+let test_fixtures =
+  "test pcre features" >:::
+  [ "test [:blank:] class" >:: test_blank_class ]
+
+let _ = run_test_tt_main test_fixtures
+
