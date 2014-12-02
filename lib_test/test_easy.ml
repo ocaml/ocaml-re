62fefa7b8e22c9b54b495b61454d52a6b3f8c257
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-12-02 14:04:31 +0100

    add some tests for Re.Easy

---
+++ lib_test/test_easy.ml
@@ -0,0 +1,39 @@
+(* Tests for Re.Easy *)
+
+open OUnit
+
+let pp_str x = x
+let pp_list l = "[" ^ String.concat ", " l ^ "]"
+
+let re_whitespace = Re_posix.compile_pat "[\t ]+"
+
+let test_iter () =
+  let re = Re_posix.compile_pat "(ab)+" in
+  assert_equal ~printer:pp_list
+    ["abab"; "ab"; "ab"] (Re.Easy.iter_matched re "aabab aaabba  dab ");
+  assert_equal ~printer:pp_list
+    ["ab"; "abab"] (Re.Easy.iter_matched ~pos:2 ~len:7 re "abab ababab");
+  ()
+
+let test_split () =
+  assert_equal ~printer:pp_list
+    ["aa"; "bb"; "c"; "d"] (Re.Easy.split re_whitespace "aa bb c d ");
+  assert_equal ~printer:pp_list
+    ["a"; "b"] (Re.Easy.split ~pos:1 ~len:4 re_whitespace "aa b c d");
+  ()
+
+let test_replace () =
+  let re = Re_posix.compile_pat "[a-zA-Z]+" in
+  let f sub = String.capitalize (Re.get sub 0) in
+  assert_equal ~printer:pp_str  " Hello World; I Love Chips!"
+    (Re.Easy.replace re ~f " hello world; I love chips!");
+  ()
+
+let suite = "easy" >:::
+  [ "iter" >:: test_iter
+  ; "split" >:: test_split
+  ; "replace" >:: test_replace
+  ]
+
+let () =
+  ignore (run_test_tt_main suite)
--- lib_test/test_exec_iter.ml
+++ lib_test/test_exec_iter.ml
@@ -1,13 +1,18 @@
 #!/usr/bin/env ocaml
 #use "topfind";;
-#require "sequence";;
 #directory "_build/lib";;
 #load "re.cma";;
 #load "re_posix.cma";;
 let re = Re_posix.re "a*(ab)" |> Re.compile;;
 let s = "aaaaabaaa aaabaaabaaaabaa axbaaba";;
-Re.exec_iter re s
-  |> Sequence.iter
+
+let rec iter_gen f g = match g() with
+  | None -> ()
+  | Some x -> f x; iter_gen f g
+;;
+
+Re.Easy.iter_gen re s
+  |> iter_gen
     (fun s ->
       let i,j = Re.get_ofs s 0 in
       Printf.printf "%s at %d,%d\n" (Re.get s 0) i j
