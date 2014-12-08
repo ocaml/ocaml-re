7d7d0cc66de0d57c731b0a140a89c5c6b8d15f44
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-12-08 16:40:48 -0500

    pcre tests compatible with 4.01

--- lib_test/test_pcre.ml
+++ lib_test/test_pcre.ml
@@ -1,14 +1,14 @@
 open OUnit
 
 let test_blank_class _ =
-  let re = Re_perl.compile_pat {|\d[[:blank:]]\d[[:blank:]]+[a-z]|} in
+  let re = Re_perl.compile_pat "|\\d[[:blank:]]\\d[[:blank:]]+[a-z]|" in
   let successes = ["1 2  a"; "2\t3 z"; "9\t0 \t a"] in
   let failures = [""; "123"; "  "; "1 3z"] in
   successes |> List.iter (fun s ->
     assert_bool ("String " ^ s ^ " should match") (Re.execp re s)
   );
   failures |> List.iter (fun s ->
-    assert_bool ("String " ^ s ^ " should not match") (not @@ Re.execp re s)
+    assert_bool ("String " ^ s ^ " should not match") (not (Re.execp re s))
   )
 
 
