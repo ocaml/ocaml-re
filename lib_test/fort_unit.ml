40953ed78adacbd5e220aad3cec4ac5e30b90bd8
Rudi Grinberg <rudi.grinberg@gmail.com>
2016-08-02 21:12:20 -0400

    Improve Fort_unit errors
    Always print exceptions.

--- lib_test/fort_unit.ml
+++ lib_test/fort_unit.ml
@@ -5,16 +5,22 @@ type ('a, 'b) either =
   | Left of 'a
   | Right of 'b
 
+let str_of_either f g = function
+  | Left a -> f a
+  | Right b -> g b
+
 let try_with f = try Right (f ()) with exn -> Left exn
 
 let expect_equal_app ?printer ?msg f x g y =
   let fx = try_with (fun () -> f x) in
   let gy = try_with (fun () -> g y) in
-  match fx, gy with
-  | Right fx, Right gy -> assert_equal ?printer ?msg fx gy
-  | Left e1, Left e2 -> assert_equal ~printer:(Printexc.to_string) ?msg e1 e2
-  | Left _, Right _ -> assert_failure "f x raised while g y didn't"
-  | Right _, Left _ -> assert_failure "g y raised while f x didn't"
+  let printer =
+    let right x =
+      match printer with
+      | None -> "<no printer>"
+      | Some p -> p x in
+    str_of_either Printexc.to_string right in
+  assert_equal ~printer ?msg fx gy
 
 let collected_tests = ref []
 
@@ -29,9 +35,13 @@ let list_printer f l =
    "[" ^ (String.concat "; " (List.map f l)) ^ "]"
 let arr_printer f a =
    "[|" ^ (String.concat "; " (List.map f (Array.to_list a))) ^ "|]"
+let opt_printer f = function
+  | None -> "<None>"
+  | Some s -> "Some (" ^ (f s) ^ ")"
 
 let arr_str_printer = arr_printer str_printer
 let arr_ofs_printer = arr_printer ofs_printer
+let list_ofs_printer = list_printer ofs_printer
 
 let fail = assert_failure
 
@@ -45,6 +55,9 @@ let expect_eq_arr_str ?msg f x g y =
   expect_equal_app ?msg ~printer:arr_str_printer f x g y
 let expect_eq_arr_ofs  ?msg f x g y =
   expect_equal_app ?msg ~printer:arr_ofs_printer f x g y
+
+let expect_eq_list_str ?msg f x g y =
+  expect_equal_app ?msg ~printer:(list_printer str_printer) f x g y
 
 let expect_pass name run =
   collected_tests := (name >:: (fun _ -> run ())) :: !collected_tests
