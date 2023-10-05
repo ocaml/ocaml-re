de7057cab6d7beddc467cd7ff8b12fe2c96a7bf4
Rudi Grinberg <me@rgrinberg.com>
2023-10-05 16:38:58 -0600

    chore: switch to flakes
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "ocaml-re flake";
+
+  inputs.nix-filter.url = "github:numtide/nix-filter";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        inherit (pkgs.ocamlPackages) buildDunePackage;
+      in
+      rec {
+        packages = rec {
+          default = re;
+          re = buildDunePackage {
+            pname = "re";
+            version = "n/a";
+            src = ./.;
+            duneVersion = "3";
+            propagatedBuildInputs = with pkgs.ocamlPackages; [ seq ];
+            checkInputs = with pkgs.ocamlPackages; [ ounit ];
+            doCheck = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = pkgs.lib.attrValues packages;
+          buildInputs = with pkgs.ocamlPackages; [ ocaml-lsp core_bench ];
+        };
+      });
+}
