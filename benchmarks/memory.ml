4d5a5172c921e03b08be60c9272333cbd0c4cfe1
Rudi Grinberg <me@rgrinberg.com>
2024-04-28 19:26:19 +0100

    chore: improve memory benchmarks
    Include another pathological case

--- benchmarks/memory.ml
+++ benchmarks/memory.ml
@@ -11,16 +11,21 @@ let re =
   compile @@ seq [ rep (set "01"); char '1'; repn (set "01") size (Some size) ]
 ;;
 
+(* Another pathological case that is a simplified version of the above *)
+let re2 =
+  let open Re in
+  seq [ rep (set "01"); char '1'; repn (set "01") size (Some size); char 'x' ] |> compile
+;;
+
 let str = "01" ^ String.make size '1'
 
 let benchmarks =
-  Bench.Test.create_indexed
-    ~name:"pathological re"
-    ~args:[ 10; 20; 40; 80; 100; size ]
-    (fun len ->
-       Base.Staged.stage (fun () ->
-         let len = Base.Int.min (String.length str) len in
-         ignore (Re.execp ~pos:0 ~len re str)))
+  [ "re", re; "re2", re2 ]
+  |> ListLabels.map ~f:(fun (name, re) ->
+    Bench.Test.create_indexed ~name ~args:[ 10; 20; 40; 80; 100; size ] (fun len ->
+      Base.Staged.stage (fun () ->
+        let len = Base.Int.min (String.length str) len in
+        ignore (Re.execp ~pos:0 ~len re str))))
 ;;
 
-let () = Command_unix.run (Bench.make_command [ benchmarks ])
+let () = Command_unix.run (Bench.make_command benchmarks)
