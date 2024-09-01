7ef3d99e667c1c6755af0efdf176797a33168103
Rudi Grinberg <me@rgrinberg.com>
2024-09-01 14:02:33 +0100

    chore: restore 4.14 compat (#388)

--- lib/fmt.ml
+++ lib/fmt.ml
@@ -5,7 +5,7 @@ include Format
 type 'a t = Format.formatter -> 'a -> unit
 
 let list = pp_print_list
-let array = pp_print_array
+let array ?pp_sep f fmt arr = list ?pp_sep f fmt (Array.to_list arr)
 let str = pp_print_string
 let sexp fmt s pp x = fprintf fmt "@[<3>(%s@ %a)@]" s pp x
 let bytes fmt t = Format.fprintf fmt "%S" (Bytes.to_string t)
