08b79678f46c04aed4cb0ff9ea0525e92b7dc827
Rudi Grinberg <me@rgrinberg.com>
2024-09-21 09:56:00 +0100

    refactor: use unboxed options
    Save status as unboxed

--- lib/import.ml
+++ lib/import.ml
@@ -7,3 +7,35 @@ end
 
 let ( = ) = Int.equal
 let compare = Int.compare
+
+module Option = struct
+  include Option
+
+  module Unboxed : sig
+    type 'a t
+
+    val none : 'a t
+    val some : 'a -> 'a t
+    val is_none : 'a t -> bool
+    val is_some : 'a t -> bool
+    val value_exn : 'a t -> 'a
+    val to_option : 'a t -> 'a option
+    val iter : 'a t -> f:('a -> unit) -> unit
+  end = struct
+    type 'a t = Obj.t
+
+    let some x = Obj.repr x
+    let none = Obj.repr (Sys.opaque_identity (-1))
+    let phys_equal = Stdlib.( == )
+    let is_none t = phys_equal t none
+    let is_some t = not (phys_equal t none)
+
+    let value_exn t =
+      if is_none t then invalid_arg "Option.Unboxed.value_exn called on None";
+      Obj.obj t
+    ;;
+
+    let to_option t = if is_none t then None else Some (value_exn t)
+    let iter t ~f = if is_none t then () else f (value_exn t)
+  end
+end
