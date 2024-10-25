de9bd965cb2a8cf525d1b968ea957d07833c0c91
Rudi Grinberg <me@rgrinberg.com>
2024-10-25 19:52:30 +0100

    refactor: rename constructor to variant (#545)

--- lib/dyn.ml
+++ lib/dyn.ml
@@ -3,10 +3,10 @@ type t =
   | Tuple of t list
   | Enum of string
   | List of t list
-  | Constructor of string * t list
+  | Variant of string * t list
   | Record of (string * t) list
 
-let variant x y = Constructor (x, y)
+let variant x y = Variant (x, y)
 let list x = List x
 let int x = Int x
 let pair x y = Tuple [ x; y ]
