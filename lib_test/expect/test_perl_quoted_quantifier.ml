1f5a8295f177ccaa180d7716ecef89d899c9bfd0
Rudi Grinberg <me@rgrinberg.com>
2026-09-20 16:03:38 +0100

    test(perl): snapshot quantification of quoted metacharacters
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ lib_test/expect/test_perl_quoted_quantifier.ml
@@ -0,0 +1,8 @@
+open Import
+
+let%expect_test "a quantifier applies to the last quoted metacharacter" =
+  (* The final + quantifies the literal +, so the match should be "a+++". *)
+  let re = Re.Perl.compile_pat {|\Qa+\E+|} in
+  printf "%S\n" (Re.Group.get (Re.exec re "a+++") 0);
+  [%expect {| "a+" |}]
+;;
