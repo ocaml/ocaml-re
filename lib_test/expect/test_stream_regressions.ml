0df68c0f4b45cbd3d576176dbfca700e7c2fcf06
Rudi Grinberg <me@rgrinberg.com>
2026-09-20 16:03:38 +0100

    test(stream): snapshot finalization, bounds and saved-match regressions
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ lib_test/expect/test_stream_regressions.ml
@@ -0,0 +1,73 @@
+open Import
+module Stream = Re.Stream
+
+let ok = function
+  | Stream.Ok x -> x
+  | No_match -> failwith "unexpected No_match"
+;;
+
+let group_stream re = Stream.Group.create (Stream.create re)
+
+let%expect_test "group finalization recognizes the last newline" =
+  let re = Re.compile Re.(seq [ str "a"; leol ]) in
+  printf "exec: %b\n" (Re.execp re "a\n");
+  (match Stream.Group.finalize (group_stream re) "a\n" ~pos:0 ~len:2 with
+   | No_match -> print_endline "stream: No_match"
+   | Ok m ->
+     Format.printf "stream: %a@." (Fmt.opt Fmt.quoted_string) (Stream.Group.Match.get m 0));
+  [%expect
+    {|
+    exec: true
+    stream: No_match
+    |}]
+;;
+
+let%expect_test "a fed final newline is retained until finalization" =
+  let re = Re.compile Re.(seq [ str "a"; leol ]) in
+  printf "exec: %b\n" (Re.execp re "a\n");
+  let t = ok (Stream.feed (Stream.create re) "a\n" ~pos:0 ~len:2) in
+  printf "stream: %b\n" (Stream.finalize t "" ~pos:0 ~len:0);
+  [%expect
+    {|
+    exec: true
+    stream: false
+    |}]
+;;
+
+let%expect_test "the last newline is relative to the finalized slice" =
+  let re = Re.compile Re.(seq [ str "a"; leol ]) in
+  List.iter [ "a\n"; "a\nx" ] ~f:(fun s ->
+    printf "%S: %b\n" s (Stream.finalize (Stream.create re) s ~pos:0 ~len:2));
+  [%expect
+    {|
+    "a\n": true
+    "a\nx": false
+    |}]
+;;
+
+let%expect_test "negative chunk lengths must be rejected" =
+  let t = Stream.create (Re.compile Re.epsilon) in
+  invalid_argument (fun () ->
+    match Stream.feed t "" ~pos:0 ~len:(-1) with
+    | Ok _ -> print_endline "Ok"
+    | No_match -> print_endline "No_match");
+  [%expect {| Ok |}]
+;;
+
+let%expect_test "a saved group match survives reuse of its stream" =
+  let t = group_stream (Re.compile Re.(whole_string (rep any))) in
+  let m = ok (Stream.Group.finalize t "a" ~pos:0 ~len:1) in
+  let print () =
+    match Stream.Group.Match.get m 0 with
+    | result -> Format.printf "%a@." (Fmt.opt Fmt.quoted_string) result
+    | exception Assert_failure _ -> print_endline "Assert_failure"
+  in
+  print ();
+  ignore (ok (Stream.Group.finalize t "abc" ~pos:0 ~len:3));
+  print ();
+  [%expect
+    {|
+    "a"
+    Assert_failure
+    |}]
+;;
