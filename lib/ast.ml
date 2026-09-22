6a1320af99560b27d3b84c063e5d93e3d84cff5b
Rudi Grinberg <me@rgrinberg.com>
2026-09-22 22:01:21 +0100

    fix(core): retain capture declarations in zero-count repetitions
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lib/ast.ml
+++ lib/ast.ml
@@ -278,7 +278,6 @@ module Export = struct
     if i < 0 then invalid_arg "Re.repn";
     match j, i with
     | Some j, _ when j < i -> invalid_arg "Re.repn"
-    | Some 0, 0 -> epsilon
     | Some 1, 1 -> r
     | _ -> Repeat (r, i, j)
   ;;
